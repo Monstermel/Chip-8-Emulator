e063e12895f5aa95f06137e63ff4791a52d755ef
Monstermel <57382041+Monstermel@users.noreply.github.com>
2025-12-11 12:56:35 -0600

    feat: Update Utility.hpp for better naming

--- include/chip_8/utility.hpp
+++ include/chip_8/utility.hpp
@@ -5,20 +5,20 @@
 
 namespace emu {
 
-inline std::uint8_t getNibbleX(const std::uint16_t instruction) {
-    return (instruction & 0x0F00) >> 8;
+inline std::uint8_t getNibbleX(const std::uint16_t bytecode) {
+    return (bytecode & 0x0F00) >> 8;
 }
 
-inline std::uint8_t getNibbleY(const std::uint16_t instruction) {
-    return (instruction & 0x00F0) >> 4;
+inline std::uint8_t getNibbleY(const std::uint16_t bytecode) {
+    return (bytecode & 0x00F0) >> 4;
 }
 
-inline std::uint8_t getLowByte(const std::uint16_t instruction) {
-    return instruction & 0x00FF;
+inline std::uint8_t getLowByte(const std::uint16_t bytecode) {
+    return bytecode & 0x00FF;
 }
 
-inline std::uint16_t getAddress(const std::uint16_t instruction) {
-    return instruction & 0x0FFF;
+inline std::uint16_t getAddress(const std::uint16_t bytecode) {
+    return bytecode & 0x0FFF;
 }
 
 };  // namespace emu
